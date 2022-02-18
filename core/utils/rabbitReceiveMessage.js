817b521712fbe8a5e05b99fef41829f166f1ff54
mahesh-muttinti <maheshmuttinti@gmail.com>
2022-02-18 16:43:12 +0530

    update: nobejs version

--- core/utils/rabbitReceiveMessage.js
+++ core/utils/rabbitReceiveMessage.js
@@ -1,20 +1,37 @@
 const amqp = require("amqplib");
+const Sentry = require("@sentry/node");
 
 const rabbitReceiveMessage = async (amqp_endpoint, q, storyName) => {
   var conn;
   try {
+    console.log("Listen to:", q);
     conn = await amqp.connect(amqp_endpoint);
     const ch = await conn.createChannel();
     var ok = await ch.assertQueue(q, { durable: true });
     ch.consume(q, async (msg) => {
       if (msg !== null) {
-        await queueJobStrategy(storyName, {
-          job: JSON.parse(msg.content.toString()),
-        });
-        await ch.ack(msg);
+        try {
+          await queueJobStrategy(storyName, {
+            job: JSON.parse(msg.content.toString()),
+          });
+          await ch.ack(msg);
+        } catch (error) {
+          if (process.env.SENTRY_DSN !== "" && process.env.DEBUG !== "true") {
+            Sentry.captureException(error);
+          }
+
+          if (process.env.DEBUG === "true") {
+            console.log("rabbitReceiveMessage:error", error);
+          }
+
+          if (error.ack) {
+            await ch.ack(msg);
+          }
+        }
       }
     });
   } catch (error) {
+    throw error;
   } finally {
   }
 };
--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const httpServer = requireHttpServer();
 
 const server = httpServer({});
 
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException("error", e);
+  } finally {
+    console.log("backend-discuss");
+    transaction.finish();
+  }
+}, 99);
+
 server.listen(process.env.PORT || 3000, "0.0.0.0", (err, address) => {
   if (err) {
     console.log(err);
